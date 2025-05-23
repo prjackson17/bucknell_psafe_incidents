c2b08e4e5c04dc0cdfdd094cdd5d4f34e7aa59c5
Parker Jackson <prj004@bucknell.edu>
2025-05-23 13:57:37 -0400

    Update app.run configuration to use environment port and host settings

--- app.py
+++ app.py
@@ -52,4 +52,5 @@ def monthly_stats():
     return jsonify(sorted_data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
